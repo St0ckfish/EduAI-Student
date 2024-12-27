18cb9410cc97a1a20297ba4198ae63e0e58de413
M0staphaTaha <darsh.taha12382@gmail.com>
2024-12-27 13:23:16 +0200

    fix bug

--- src/APIs/axios.ts
+++ src/APIs/axios.ts
@@ -1,16 +1,20 @@
 import axios from "axios";
 import Cookies from "js-cookie";
+export const baseUrlStock = `wss://api.eduai.tech/`; //wss://eduai.vitaparapharma.com //wss://195.35.28.106:8091 //wss://api.eduai.tech
 
 const axiosInstance = axios.create({
-  baseURL: "https://eduai.vitaparapharma.com",
-  // baseURL: "https://api.eduai.tech",
+  baseURL: "https://api.eduai.tech/",
 });
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = Cookies?.get("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    const isAuthRoute = config.url?.includes('/login') ?? config.url?.includes('/signup');
+    
+    if (!isAuthRoute) {
+      const token = Cookies?.get("token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
     }
     return config;
   },
